3b4609212fd01c220b75a26506743525fdcad534
Sebastian <info@zweizeichen.org>
2017-03-19 09:47:47 +0100

    Switched to maintained CORS middleware

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 
 	"github.com/Sirupsen/logrus"
 	"github.com/boltdb/bolt"
-	"github.com/gin-gonic/contrib/cors"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/contrib/ginrus"
 	"github.com/gin-gonic/gin"
 	"github.com/kelseyhightower/envconfig"
