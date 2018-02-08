68449347896eb7e9198d36f00edddf8e92b9488c
Sebastian <info@zweizeichen.org>
2018-02-08 14:16:09 +0100

    Updated protobuf build

--- generateProto.sh
+++ generateProto.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 mkdir -p ./lib/staticData
 
-protoc -I/usr/local/include -I. \
--I../element43/services/staticData \
--I$GOPATH/src \
+go get -u github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway
+go get -u github.com/golang/protobuf/protoc-gen-go
+
+protoc -I../element43/services/staticData \
 -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
 --go_out=plugins=grpc:./lib/staticData \
 ../element43/services/staticData/staticData.proto
